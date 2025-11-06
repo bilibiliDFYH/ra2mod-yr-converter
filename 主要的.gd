14286c43f878aa59752afc551bbcef4fb8512b82
bilibiliDFYH <df_yin_hu@126.com>
2025-11-06 11:59:15 +0800

    Update 主要的.gd

--- 主要的.gd
+++ 主要的.gd
@@ -1,6 +1,7 @@
 extends Node
 
-var mod路径 = "X:\\游戏\\红警\\重聚\\RF-Client-master\\Bin\\Mod&AI\\Mod\\RA2"
+var mod路径 = OS.get_executable_path ().get_base_dir () + "\\mod路径"
+var 输出路径 = OS.get_executable_path ().get_base_dir () + "\\输出路径"
 var 复制的文件列表 = ["ai.ini" , "art.ini" , "rules.ini" , "credits.txt"]
 
 var rulesmd
@@ -27,8 +28,31 @@ func _ready () -> void :
 	退出按钮.mouse_entered.connect(func(): 退出按钮.texture = load("res://按钮-点击.png") )
 	退出按钮.mouse_exited.connect(func(): 退出按钮.texture = load("res://按钮.png") )
 
+	var 配置文件 = 读取ini ("配置文件.ini" , true)
+	print(配置文件)
+
+	for temp_string1 in 配置文件 :
+		var temp_string1_键 = temp_string1.substr (0 , temp_string1.find ("=") )
+		var temp_string1_值 = temp_string1.substr (temp_string1.find ("=") + 1)
+		match temp_string1_键 :
+			"mod路径" :
+				mod路径 = temp_string1_值.rstrip("\\/")
+			"输出路径" :
+				输出路径 = temp_string1_值
+			"复制的文件列表" :
+				复制的文件列表 = temp_string1_值.split ( "," , false )
+			"一次处理量" :
+				一次处理多少个 = int (temp_string1_值)
+			"是否自动退出" :
+				temp_string1_值 = 文本变为小写字母 (temp_string1_值)
+				if temp_string1_值 == "yes" || temp_string1_值 == "true" :
+					是否自动退出 = true
+				elif temp_string1_值 == "no" || temp_string1_值 == "false" :
+					是否自动退出 = false
+
 	for i in 7 :
 		进度条.append (Vector2i (0 , 0) )
+
 	call_deferred ("开始修改")
 
 func _process(delta: float) -> void:
@@ -53,36 +77,17 @@ func 开始修改 () :
 	await get_tree().process_frame
 	await get_tree().process_frame
 
-	var 配置文件 = ReadINI ("配置文件.ini" , true)
-
-	for temp_string1 in 配置文件 :
-		var temp_string1_键 = temp_string1.substr (0 , temp_string1.find ("=") )
-		var temp_string1_值 = temp_string1.substr (temp_string1.find ("=") + 1)
-		if temp_string1_键 == "mod路径" :
-			mod路径 = temp_string1_值
-		if temp_string1_键 == "复制的文件列表" :
-			复制的文件列表 = temp_string1_值.split ( "," , false )
-		if temp_string1_键 == "一次处理量" :
-			一次处理多少个 = int (temp_string1_值)
-		if temp_string1_键 == "是否自动退出" :
-			temp_string1_值 = 文本变为小写字母 (temp_string1_值)
-			if temp_string1_值 == "yes" || temp_string1_值 == "true" :
-				是否自动退出 = true
-			elif temp_string1_值 == "no" || temp_string1_值 == "false" :
-				是否自动退出 = false
-
-	while mod路径.substr (mod路径.length () - 1) == "\\" || mod路径.substr (mod路径.length () - 1) == "/" :
-		mod路径 = mod路径.substr (0 , mod路径.length () - 1)
-
 #复制文件
 	for 文件名 in 复制的文件列表 :
 		var 新文件名 = 文件名.substr (0 , 文件名.find (".") ) + "md" + 文件名.substr (文件名.find (".") )
-		if FileAccess.file_exists (mod路径 + "\\" + 文件名) && !FileAccess.file_exists (mod路径 + "\\" + 新文件名):
-			DirAccess.copy_absolute (mod路径 + "\\" + 文件名 , mod路径 + "\\" + 新文件名)
+		if not DirAccess.dir_exists_absolute(输出路径):
+			DirAccess.make_dir_recursive_absolute(输出路径)
+		if FileAccess.file_exists (mod路径 + "\\" + 文件名) :
+			DirAccess.copy_absolute (mod路径 + "\\" + 文件名 , 输出路径 + "\\" + 新文件名)
 
 #改rules
-	rulesmd = ReadINI (mod路径 + "\\rules.ini")
-	rulesmd_yr = ReadINI ("res://rulesmd-yr.ini")
+	rulesmd = 读取ini (mod路径 + "\\rules.ini")
+	rulesmd_yr = 读取ini ("res://rulesmd-yr.ini")
 
 	处理__General_小节 ()
 	var sides = 处理__Sides_小节 ()
@@ -93,83 +98,82 @@ func 开始修改 () :
 	处理__AI_小节 ()
 
 func 保存文件 () :
-	rulesmd = INI_Merge_Sections (rulesmd)
+	rulesmd = 合并ini (rulesmd)
 	rulesmd = rulesmd.filter (func (element) : return element != "")
-	DirAccess.open(mod路径).remove("rulesmd.ini")
-	var file = FileAccess.open(mod路径 + "\\rulesmd.ini", FileAccess.WRITE)
+	DirAccess.open(输出路径).remove("rulesmd.ini")
+	var file = FileAccess.open(输出路径 + "\\rulesmd.ini", FileAccess.WRITE)
 	for 当前行 in rulesmd :
-		file.store_string(当前行 + "\n")
+		file.store_string(当前行 + "\r\n")
 	file.close()
-	print("保存文件")
 
 	if 是否自动退出 : get_tree().quit()
 
-#读取ini
-func ReadINI (Path_ini , 是根目录 = false) :											#输入ini类型名称和ini文件位置
+func 读取ini (Path_ini , 是根目录 = false) :
 	if 是根目录 : Path_ini = OS.get_executable_path ().get_base_dir () + "\\" + Path_ini
 	var file_ini = FileAccess.open (Path_ini , FileAccess.READ )	#读取文件
 	var 临时_ini_主要的 = file_ini.get_as_text ()
 	file_ini.close ()
-	临时_ini_主要的 = 临时_ini_主要的.replace ("\t" , "")
-	临时_ini_主要的 = 临时_ini_主要的.replace ("\r" , "")
-	临时_ini_主要的 = 临时_ini_主要的.split ( "\n" , false )
 
-	for i in 临时_ini_主要的.size () :											#删注释和空格
-		var temp_str_1 = 临时_ini_主要的[i]
-		if temp_str_1.find (";") != -1 :
-			temp_str_1 = temp_str_1.substr (0 , temp_str_1.find (";"))
+	#删制表符、回车符，并根据换行符拆分成列表
+	临时_ini_主要的 = Array (临时_ini_主要的.split ( "\n" , false ) )
+	临时_ini_主要的 = 临时_ini_主要的.map(func(行): return 行.replace("\t", "").replace("\r", "") )
+	#删制注释和空格
+	临时_ini_主要的 = 临时_ini_主要的.map(func(行) :
+		if 行.find (";") != -1 :
+			行 = 行.substr (0 , 行.find (";"))
+		if 行.substr (0 , 行.find ("=") ) != "Name" :
+			行 = 行.replace (" " , "")
+		return 行 )
+	#删制空行
+	临时_ini_主要的 = 临时_ini_主要的.filter (func (element) : return element != "")
 
-		temp_str_1 = temp_str_1.replace (" " , "")
-
-		临时_ini_主要的[i] = temp_str_1
-	for i in 临时_ini_主要的.size () :											#去掉空行
-		if i > 临时_ini_主要的.size () - 1 :
-			break
-		if 临时_ini_主要的[i] == "" || 临时_ini_主要的[i] == "\n" || 临时_ini_主要的[i] == "\r" || 临时_ini_主要的[i] == "\r\n" || 临时_ini_主要的[i] == "\n\r" :
-			临时_ini_主要的.remove_at (i)
-	临时_ini_主要的 = INI_Merge_Sections (临时_ini_主要的)
+	临时_ini_主要的 = 合并ini (临时_ini_主要的)
 	return 临时_ini_主要的
 
-#合并ini
-func INI_Merge_Sections (临时_ini_主要的) :
-	var ini_小节列表 = []
-	var 临时_ini_主要的_1 = 临时_ini_主要的
-	临时_ini_主要的 = []
-	for i in 临时_ini_主要的_1.size () :											#找到所有小节
-		if 临时_ini_主要的_1[i].find("=") == -1 && 临时_ini_主要的_1[i].find("[") == 0 && 临时_ini_主要的_1[i].find("]") == 临时_ini_主要的_1[i].length() - 1 :
-			if 临时_ini_主要的_1[i] != "[]" :
-				ini_小节列表.append (i)
+func 合并ini (临时_ini_主要的) :
+	var 小节列表 = []
+	var 临时_ini_主要的_1 = []
+	for temp_string_1 in 临时_ini_主要的 :
+		if temp_string_1.find ("=") == -1 && temp_string_1.find ("[") != -1 && temp_string_1.find ("]") != -1 && temp_string_1 != "[]" :
+			temp_string_1 = temp_string_1.substr (temp_string_1.find ("[") + 1)
+			temp_string_1 = temp_string_1.substr (0 , temp_string_1.find ("]") )
+			if 小节列表.find (temp_string_1) == -1 :
+				小节列表.append(temp_string_1)
+	for 小节 in 小节列表 :
+		临时_ini_主要的_1.append ("["+ 小节 +"]")
+		var 小节内容_1 = []
+		while 临时_ini_主要的.find ("["+ 小节 +"]") != -1 :
+			小节内容_1 += 获取小节 (临时_ini_主要的 , 小节)
+			临时_ini_主要的 = 删除小节 (临时_ini_主要的 , 小节)
 
-	for i in ini_小节列表.size () :
-		var temp_a = ini_小节列表[i]
-		if 临时_ini_主要的.find (临时_ini_主要的_1[temp_a]) == -1 :					#添加节头
-			临时_ini_主要的.append (临时_ini_主要的_1[temp_a])
-		var temp_b = 1
-		var temp_INI_main_find = 临时_ini_主要的.find (临时_ini_主要的_1[temp_a])
-		var temp_INI_main_find_1
-		while true :
-			if temp_INI_main_find + temp_b >= 临时_ini_主要的.size () :
-				temp_INI_main_find_1 = temp_INI_main_find
-				break
-			elif 临时_ini_主要的[temp_INI_main_find + temp_b].find("[") != -1 :
-				temp_INI_main_find_1 = temp_INI_main_find + temp_b - 1
-				break
-			temp_b = temp_b + 1
+		var result_dict = {}
+		for line in 小节内容_1:
+			# 跳过空行
+			if line.strip_edges().is_empty():
+				continue
+				
+			# 分割键值对
+			if "=" in line:
+				var parts = line.split("=", false, 1)
+				var key = parts[0].strip_edges()
+				var value = parts[1].strip_edges() if parts.size() > 1 else ""
+				
+				# 相同的键，后面的值覆盖前面的
+				result_dict[key] = value
+		
+		# 转换回数组格式
+		var result_array = []
+		for key in result_dict:
+			result_array.append("%s=%s" % [key, result_dict[key]])
+		临时_ini_主要的_1 += result_array
 
-		temp_b = 1
-		var temp_list_1 = []
-		while temp_b + temp_a < 临时_ini_主要的_1.size () :
-			if 临时_ini_主要的_1[temp_b + temp_a].find("[") != -1:
-				break
-			temp_list_1.append(临时_ini_主要的_1[temp_b + temp_a])
-			temp_b = temp_b + 1
-		for a in temp_list_1.size () :
-			临时_ini_主要的.insert (temp_INI_main_find_1 + 1 + a , temp_list_1 [a] )
+	临时_ini_主要的_1 = Array (临时_ini_主要的_1)
 
-	return 临时_ini_主要的
+	return 临时_ini_主要的_1
 
-func 显示文本进度条 (物体 , 文本 , 进度条) :
-	物体.text = 文本 + str(进度条.x) + "/" + str(进度条.y)
+
+func 显示文本进度条 (物体 , 文本 , 进度条_1) :
+	物体.text = 文本 + str(进度条_1.x) + "/" + str(进度条_1.y)
 
 func 文本变为小写字母 (文本) :
 	文本 = 文本.replace ("A" , "a").replace ("B" , "b").replace ("C" , "c").replace ("D" , "d")
@@ -182,19 +186,19 @@ func 文本变为小写字母 (文本) :
 	文本 = 文本.replace ("X" , "x").replace ("Y" , "y").replace ("Z" , "z")
 	return 文本
 
-func 删除小节 (小节名称) :
-	rulesmd = INI_Merge_Sections (rulesmd)
-	var 小节位置 = rulesmd.find ("[" + 小节名称 + "]")
-	if 小节位置 == -1 : return rulesmd
-	rulesmd.remove_at (小节位置)
-	while 小节位置 < rulesmd.size () :
-		var temp_string1 = rulesmd[小节位置]
+
+func 删除小节 (ini文件 , 小节名称) :
+	var 小节位置 = ini文件.find ("[" + 小节名称 + "]")
+	if 小节位置 == -1 : return ini文件
+	ini文件.remove_at (小节位置)
+	while 小节位置 < ini文件.size () :
+		var temp_string1 = ini文件[小节位置]
 		if temp_string1.find ("=") == - 1 && temp_string1.find ("[") != -1 :
 			break
-		rulesmd.remove_at (小节位置)
+		ini文件.remove_at (小节位置)
+	return ini文件
 
 func 获取小节 (ini文件 , 小节名称) :
-	ini文件 = INI_Merge_Sections (ini文件)
 	var 小节位置 = ini文件.find ("[" + 小节名称 + "]")
 	var i = 1
 	var 新的小节 = []
@@ -206,12 +210,15 @@ func 获取小节 (ini文件 , 小节名称) :
 		i += 1
 	return 新的小节
 
+
 func 处理__General_小节 () :
-	进度条[0] = Vector2i (0 , 2)
+	进度条[0] = Vector2i (0 , 3)
+	rulesmd = 合并ini (rulesmd)
+	var 旧的小节 = 获取小节 (rulesmd , "General")
 
-	删除小节 ("General")
+	rulesmd = 删除小节 (rulesmd , "General")
 
-	进度条[0] = Vector2i (1 , 2)
+	进度条[0] = Vector2i (1 , 3)
 
 	var 新的小节 = 获取小节 (rulesmd_yr , "General")
 
@@ -224,12 +231,15 @@ func 处理__General_小节 () :
 	temp_ini.append ("PermaControlledAnimationType=MINDANIMR")
 	temp_ini.append ("MindControlAttackLineFrames=20")
 	rulesmd += temp_ini
+	进度条[0] = Vector2i (2 , 3)
 
 	temp_ini = ["[General]"]
 	temp_ini += 新的小节
+	temp_ini += 旧的小节
 	rulesmd += temp_ini
+	rulesmd = 合并ini (rulesmd)
 
-	进度条[0] = Vector2i (2 , 2)
+	进度条[0] = Vector2i (3 , 3)
 
 func 处理__Sides_小节 () :
 	进度条[1] = Vector2i (0 , 1)
@@ -237,7 +247,6 @@ func 处理__Sides_小节 () :
 	var 新的小节 = 获取小节 (rulesmd , "Sides")
 	var sides = []
 
-	var 临时int1 = 0
 	for temp_string1 in 新的小节 :
 		var temp_string1_键 = temp_string1.substr (0 , temp_string1.find ("=") )
 		var temp_string1_值 = temp_string1.substr (temp_string1.find ("=") + 1)
@@ -250,6 +259,7 @@ func 处理__Sides_小节 () :
 
 func 处理__InfantryTypes_小节__步兵_小节 (盟军所属 , 苏军所属) :
 	进度条[2] = Vector2i (0 , 2)
+	rulesmd = 合并ini (rulesmd)
 	var 步兵注册表 = 获取小节 (rulesmd , "InfantryTypes")
 	进度条[2] = Vector2i (1 , 2)
 
@@ -303,6 +313,7 @@ func 处理__InfantryTypes_小节__步兵_小节 (盟军所属 , 苏军所属) :
 
 func 处理__BuildingTypes_小节__建筑_小节 () :
 	进度条[4] = Vector2i (0 , 2)
+	rulesmd = 合并ini (rulesmd)
 	var 建筑注册表 = 获取小节 (rulesmd , "BuildingTypes")
 	进度条[4] = Vector2i (1 , 2)
 
@@ -371,12 +382,10 @@ func 处理__AI_小节 () :
 		if temp_string1_键 == "BuildDummy" || temp_string1_键 == "BuildDefense" || temp_string1_键 == "BuildPDefense" || temp_string1_键 == "BuildAA" :
 			var temp_ini = temp_string1_值.split ( "," , false )
 			for i in temp_ini.size () :
-				if i == 0 :
-					AlliedBaseDefenses += temp_ini[0]
-					AlliedBaseDefenses += ","
-				else :
-					SovietBaseDefenses += temp_ini[i]
-					SovietBaseDefenses += ","
+				AlliedBaseDefenses += temp_ini[0]
+				AlliedBaseDefenses += ","
+				SovietBaseDefenses += temp_ini[i]
+				SovietBaseDefenses += ","
 	进度条[6] = Vector2i (3 , 4)
 
 	var temp_ini = []
